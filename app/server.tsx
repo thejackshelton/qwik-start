0ec5f17c7c98f394de64b2a270abd30f1b2b1953
thejackshelton <me@jackshelton.com>
2025-07-18 00:30:02 -0500

    streamed html

--- app/server.tsx
+++ app/server.tsx
@@ -1,5 +1,29 @@
-export default {
-	async fetch(req: Request): Promise<Response> {
-		return new Response(`Hello world! (${req.url})`);
-	},
-};
+import { H3, defineHandler } from "h3";
+import { renderToStream } from "@qwik.dev/core/server";
+import { Counter } from "./src/components/counter";
+import { manifest } from "@qwik-client-manifest";
+
+const app = new H3();
+
+const handler = defineHandler(() => {
+	let html = "";
+
+	renderToStream(<Counter />, {
+		manifest,
+		containerTagName: "div",
+		stream: {
+			write: (chunk: string) => {
+				console.log("Chunk: ", chunk);
+				html += chunk;
+			},
+		},
+	});
+
+	console.log("HTML: ", html);
+
+	return html;
+});
+
+export default handler;
+
+app.get("/", handler);
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig, type UserConfig } from "vite";
 import { qwikVite as qwik } from "@qwik.dev/core/optimizer";
 import { nitro } from "nitro/vite";
-import { qwikEntryConfig } from "qwik-start";
 
 export default defineConfig((): UserConfig => {
 	return {
-		plugins: [nitro()],
+		plugins: [
+			nitro(),
+			qwik({
+				devSsrServer: false,
+			}),
+		],
 	};
 });

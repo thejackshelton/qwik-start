03f205a273d66b2ca522f485d718491577f25c03
thejackshelton <me@jackshelton.com>
2025-07-22 19:08:06 -0500

    working interactivity

--- app/server.tsx
+++ app/server.tsx
@@ -1,13 +1,11 @@
 import { renderToStream } from "@qwik.dev/core/server";
 import { Counter } from "./src/components/counter";
-import { manifest } from "@qwik-client-manifest";
 
 export default {
 	async fetch(req: Request) {
 		let html = "";
 
 		await renderToStream(<Counter />, {
-			manifest,
 			containerTagName: "div",
 			stream: {
 				write: (chunk: string) => {
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig, type UserConfig } from "vite";
 import { qwikVite as qwik } from "@qwik.dev/core/optimizer";
 import { nitro } from "nitro/vite";
+import { symbolMapper } from "@qwik.dev/core/optimizer";
 
 export default defineConfig((): UserConfig => {
 	return {
 		plugins: [
+			{
+				name: "get-symbol-mapper",
+				enforce: "pre",
+				configResolved(config) {
+					globalThis.symbolMapper = symbolMapper;
+				},
+			},
 			nitro({}),
 			qwik({
 				devSsrServer: false,
